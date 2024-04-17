a1b3bb915482abd5bf4cc881c251d3af34c925c7
João Pedro Matos <97900367+joao-matoz@users.noreply.github.com>
2024-04-16 21:59:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+var randomnumber1 = Math.floor(Math.random() * 6) + 1;
+var randomnumber2 = Math.floor(Math.random() * 6) + 1;
+
+switch (randomnumber1){
+
+    case 1:
+        document.querySelector(".img1").setAttribute("src", "images/dice1.png");    
+        break;
+
+    case 2:
+        document.querySelector(".img1").setAttribute("src", "images/dice2.png");    
+        break;
+
+    case 3:
+        document.querySelector(".img1").setAttribute("src", "images/dice3.png");    
+        break;
+
+    case 4:
+        document.querySelector(".img1").setAttribute("src", "images/dice4.png");    
+        break;
+    
+    case 5:
+        document.querySelector(".img1").setAttribute("src", "images/dice5.png");    
+        break;
+    
+    case 6:
+        document.querySelector(".img1").setAttribute("src", "images/dice6.png");    
+        break;
+}
+
+var randomnumber2 = Math.floor(Math.random() * 6) + 1;
+
+switch (randomnumber2){
+
+    case 1:
+        document.querySelector(".img2").setAttribute("src", "images/dice1.png");    
+        break;
+
+    case 2:
+        document.querySelector(".img2").setAttribute("src", "images/dice2.png");    
+        break;
+
+    case 3:
+        document.querySelector(".img2").setAttribute("src", "images/dice3.png");    
+        break;
+
+    case 4:
+        document.querySelector(".img2").setAttribute("src", "images/dice4.png");    
+        break;
+    
+    case 5:
+        document.querySelector(".img2").setAttribute("src", "images/dice5.png");    
+        break;
+    
+    case 6:
+        document.querySelector(".img2").setAttribute("src", "images/dice6.png");    
+        break;
+}
+
+if(randomnumber1 > randomnumber2){
+
+    document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
+}
+else if(randomnumber1 < randomnumber2){
+
+    document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+}
+else{
+
+    document.querySelector("h1").textContent = "Draw!";
+}
